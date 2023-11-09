f4781fc321fe8e779dff8849d9eef88e16d27fbb
Dorian Black <d.black.personal@gmail.com>
2023-11-09 10:13:30 +0200

    v1.1

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,16 +1,17 @@
-import { StyledTitle, StyledFilter } from "./Filter.styled";
+import { StyledTitle, StyledFilter } from './Filter.styled';
 
 export const Filter = ({ items, onChange }) => {
-     if (items.length === 0) {
-          return (
-               <>
-               <StyledTitle>There are no contacts yet!</StyledTitle>
-               </>)
-     }
-     return (
-          <>
-               <StyledTitle>Find contacts by name</StyledTitle>
-               <StyledFilter type="string" name="filter" onChange={onChange} />
-          </>
-     )
-}
+  if (items.length === 0) {
+    return (
+      <>
+        <StyledTitle>There are no contacts yet!</StyledTitle>
+      </>
+    );
+  }
+  return (
+    <>
+      <StyledTitle>Find contacts by name</StyledTitle>
+      <StyledFilter type="string" name="filter" onChange={onChange} />
+    </>
+  );
+};
